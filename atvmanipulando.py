8cf20bf40b3a43e75131e5d0b31e03d324b7d0cd
privjohxzz <joanasilvapiresdealmeida@gmail.com>
2025-11-13 08:41:09 -0300

    Add files via upload

---
+++ atvmanipulando.py
@@ -0,0 +1,5 @@
+nome = entrada('digite seu nome')
+senha = entrada('digite sua senha')
+
+com abrir("frutas.txt","um")como arquivo:
+    arquivo.escrever(nome+"\n"+senha)
---
+++ atvmanipulando2.py
@@ -0,0 +1,12 @@
+lista = [10,20,30,40,50]
+
+lista.acrescentar(60)
+
+lista.inserir(1,15)
+
+lista.remover(30)
+
+ultimo = lista.pop()
+
+imprimir("lista final:",lista)
+imprimir("ultimo numero:",ultimo)
